196de86435cae6abb618b6dd109d95e648205c24
Shivam774705 <kumarshivam774705@gmail.com>
2026-05-30 16:53:34 +0530

    fix: resolve Brotli compression scraping issue by removing Accept-Encoding

--- backend/enrichment/constants.py
+++ backend/enrichment/constants.py
@@ -26,7 +26,6 @@ DEFAULT_HEADERS = {
     "User-Agent": USER_AGENTS[0],
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
     "Cache-Control": "max-age=0"
